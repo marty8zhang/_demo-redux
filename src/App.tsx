df235edd3668d41ce7ab1580672e80405e8b8ac9
Marty Zhang <marty8zhang@gmail.com>
2021-09-19 16:31:39 +1000

    Add `react-router-dom`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FancyCounter from './pages/FancyCounter/FancyCounter';
 
 export default function App() {
   return (
-    <FancyCounter />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/fancy-counter">
+          <FancyCounter />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
